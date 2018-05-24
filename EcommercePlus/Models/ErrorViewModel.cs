7f85085efad8eebfbae611f6143e1f62703037eb
Liakat hossain <liakat89@gmail.com>
2018-05-25 03:08:05 +0600

    .net add

---
+++ EcommercePlus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcommercePlus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Blexz-ecommerce solution by BLEXZ
-Blexz-ecommerce Solution is a software to create manage online and offline shop seamlessly
-
-For business inquiries- liakat@blexz.com
